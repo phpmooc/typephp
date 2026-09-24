8b33cad5c4f9cd2be2980425f522496e9ba0bfce
tianfenghan <rango@swoole.com>
2026-09-24 15:23:16 +0800

    chore(project): bump version to 0.9.3 and update dependencies
    - Updated swoole/phpx dependency from ~2.9.1 to ~2.9.2
    - Bumped project version from 0.9.2 to 0.9.3 in project.yml
    - Updated file-version and product-version to 0.9.3 in project.yml
    - Changed VERSION constant from '0.9.2' to '0.9.3' in Translator.php

--- src/Translator.php
+++ src/Translator.php
@@ -94,7 +94,7 @@ class Translator extends Preprocessor
     use ResourceCompilationTrait;
     use ClassConstantValueTrait;
 
-    public const string VERSION = '0.9.2';
+    public const string VERSION = '0.9.3';
     public const string APP_NAME = 'TypePHP Compiler (AOT)';
     protected bool $hasExplicitOutput = false;
 
